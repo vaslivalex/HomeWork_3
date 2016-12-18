9a1afd6ffe11ccafad5ae4f4642bbb6457d6eaeb
vaslivalex <vaslivalex@mail.ru>
2016-12-19 01:17:56 +0200

    FirstCommit

---
+++ HomeWork3/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    public class Employee
+    {
+        public int ID { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public byte Age { set; get; }
+        public string PhoneNumber { set; get; }
+        public string Email { set; get; }
+
+        public Employee(int id, string firstName, string lastName, byte age, string phoneNumber, string email)
+        {
+            ID = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return ("ID: " + ID + ", фамилия: " + LastName + ", имя: " + FirstName + ", возраст: " + Age + ", номер тел.: " + PhoneNumber + ", email: " + Email);
+        }
+    }
+}
---
+++ HomeWork3/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    class Menu
+    {
+        public void Show()
+        {
+            Dictionary<string, Employee> Empl = new Dictionary<string, Employee>();
+            Empl.Add("Сотрудник1", new Employee(100, "Alex", "Ivanov", 25, "097-943-85-75", "vanya@mail.ru"));
+            Empl.Add("Сотрудник2", new Employee(101, "Petr", "Petrov", 28, "097-943-85-76", "petya@mail.ru"));
+            
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Сотрудник: ");
+            foreach (KeyValuePair<string, Employee> dev in Empl)
+            {
+                Console.Write(new String(' ', 5));
+                Console.ResetColor();
+                Console.WriteLine("{0,-10} {1,3}", dev.Key, dev.Value.ToString());
+
+            }
+        }
+    }
+}
