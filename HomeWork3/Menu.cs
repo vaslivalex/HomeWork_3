16495acdfbfb48058c7fd800ffc179919aa86973
vaslivalex <vaslivalex@mail.ru>
2016-12-19 23:28:09 +0200

    First Commit

--- HomeWork3/Menu.cs
+++ HomeWork3/Menu.cs
@@ -10,20 +10,33 @@ namespace HomeWork3
     {
         public void Show()
         {
-            Dictionary<string, Employee> Empl = new Dictionary<string, Employee>();
-            Empl.Add("Сотрудник1", new Employee(100, "Alex", "Ivanov", 25, "097-943-85-75", "vanya@mail.ru"));
-            Empl.Add("Сотрудник2", new Employee(101, "Petr", "Petrov", 28, "097-943-85-76", "petya@mail.ru"));
+            Dictionary<string, Employee> empl = new Dictionary<string, Employee>();
+            empl.Add("Сотрудник 1", new Employee(100, "Alex", "Ivanov", 25, "097-943-85-75", "vanya@mail.ru"));
+            empl.Add("Сотрудник 2", new Employee(101, "Petr", "Petrov", 28, "097-943-85-76", "petya@mail.ru"));
             
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Сотрудник: ");
-            foreach (KeyValuePair<string, Employee> dev in Empl)
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Введите команду: ");
+            Console.ResetColor();
+
+            string[] commands = Console.ReadLine().Split(' ');
+            if (commands[0].ToLower() == "exit" & commands.Length == 1)
+            {
+                return;
+            }
+            
+            foreach (var dev in empl)
             {
                 Console.Write(new String(' ', 5));
                 Console.ResetColor();
-                Console.WriteLine("{0,-10} {1,3}", dev.Key, dev.Value.ToString());
-
+                Console.WriteLine(dev.ToString());
             }
+           
+            empl.Remove("Сотрудник 1");
         }
     }
 }
